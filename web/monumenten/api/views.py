789b1d6ada6a4858ba65abf8e285b8bcd71640e6
dick <dick.kniep@lindix.nl>
2017-02-23 15:51:28 +0100

    serializer toegevoegd

--- web/monumenten/api/views.py
+++ web/monumenten/api/views.py
@@ -1,3 +1,11 @@
 # from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from monumenten.dataset.models import Monument
+
+
+#TODO selectie van de rest api?
+class MonumentList(generics.ListAPIView):
+    queryset = Monument.objects.all().prefetch_related('')
