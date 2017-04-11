c4ccbd1cd95018adced16c602e081ab2d45947d9
Thijs Creemers <thijs.creemers@gmail.com>
2017-04-11 08:31:43 +0200

    remove site reference and added quotes

--- src/monumenten/dataset/migrations/0003_geoview_monumenten.py
+++ src/monumenten/dataset/migrations/0003_geoview_monumenten.py
@@ -13,20 +13,15 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-
-        migrations.RunPython(code=create_site, reverse_code=delete_site),
-
         migrations.RunSQL(
             f"""
                 CREATE VIEW geo_monument_point AS
                 SELECT
                   m.display_naam as display,
                   cast('monumenten/monument' as varchar(30)) as type,
-                  {settings.DATAPUNT_API_URL} || 'monumenten/monumenten/' || m.id || '/' AS uri,
+                  '{settings.DATAPUNT_API_URL}' || 'monumenten/monumenten/' || m.id || '/' AS uri,
                   m.monumentcoordinaten AS geometrie
                 FROM
-                  dataset_monument m , django_site site
-                WHERE
-                  m.monumentcoordinaten IS NOT NULL and site.name = '{API_DOMAIN}';
+                  dataset_monument m;
             """)
     ]
