f78f5348534264a1633364e386b9baaa875ff3f0
Stephan Zaat <s.zaat@amsterdam.nl>
2018-01-08 14:39:54 +0100

    BAS-556 Use new import method

---
+++ src/.jenkins/import/docker-import-db.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -u   # crash on missing environment variables
+set -e   # stop on any error
+set -x   # log every command.
+
+source /.jenkins-import/docker-wait.sh
+
+# load data in database
+python manage.py migrate
+python manage.py run_import
+python manage_gevent.py run_add_missing_pand
+python manage.py run_bag_key_conversions
+python manage.py run_import_validation
+python manage.py run_refresh_materialized_views
