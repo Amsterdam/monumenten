d02aea7051ad74b734f760b25ae459c59060f455
Stephan Zaat <s.zaat@amsterdam.nl>
2018-01-08 13:17:04 +0100

    BAS-556 Use new import method

--- src/.jenkins/import/import.sh
+++ src/.jenkins/import/import.sh
@@ -9,20 +9,27 @@ dc() {
 	docker-compose -p monumenten -f ${DIR}/docker-compose.yml $*
 }
 
-trap 'dc kill ; dc rm -f' EXIT
+#trap 'dc kill ; dc rm -f' EXIT
 
 rm -rf ${DIR}/backups
 mkdir -p ${DIR}/backups
 
+echo "Building dockers"
+dc down
 dc pull
 dc build
 
-dc up -d database elasticsearch
-sleep 50
+dc up -d database
+dc up -d elasticsearch
+dc run importer /app/.jenkins/docker-wait.sh
 
 dc exec -T database update-table.sh bag bag_pand public monumenten
 dc exec -T database update-table.sh bag bag_nummeraanduiding public monumenten
 
 dc run --rm importer
-dc run --rm db-backup
-dc run --rm el-backup
+
+echo "Running backups"
+dc exec -T database backup-db.sh monumenten
+dc exec -T elasticsearch backup-indices.sh monumenten monumenten
+
+echo "Done"
--- src/.jenkins/test/docker-test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -u # crash on missing env
-set -e # stop on any error
-
-cd ../../ && ./manage.py test
