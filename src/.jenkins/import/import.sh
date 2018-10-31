6c59302673dc2622bcb36f7491068e05811ae00a
sercanyaldiz <s.yaldiz@amsterdam.nl>
2018-10-31 15:21:11 +0100

    remove the docker-netwerk when failed (#15)

--- src/.jenkins/import/import.sh
+++ src/.jenkins/import/import.sh
@@ -9,7 +9,7 @@ dc() {
 	docker-compose -p monumenten -f ${DIR}/docker-compose.yml $*
 }
 
-trap 'dc kill ; dc rm -f' EXIT
+trap 'dc kill ; dc down ; dc rm -f' EXIT
 
 rm -rf ${DIR}/backups
 mkdir -p ${DIR}/backups
@@ -31,5 +31,5 @@ dc run --rm importer
 echo "Running backups"
 dc exec -T database backup-db.sh monumenten
 dc exec -T elasticsearch backup-indices.sh monumenten monumenten
-
+dc down -v
 echo "Done"
