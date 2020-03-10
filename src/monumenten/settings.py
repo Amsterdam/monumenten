8dc15a4f1c90055c4e33e147aeb9ded9c689bbdb
Bart Heupers <bart.heupers@gmail.com>
2020-03-10 15:25:53 +0100

    Set Sentry environment to monumenten

--- src/monumenten/settings.py
+++ src/monumenten/settings.py
@@ -164,5 +164,6 @@ SENTRY_DSN = os.getenv('SENTRY_DSN')
 if SENTRY_DSN:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
+        environment="monumenten",
         integrations=[DjangoIntegration()]
     )
