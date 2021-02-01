2efc1de6cec013103815f434c7286362fba508d7
Bart Heupers <bart.heupers@gmail.com>
2021-02-01 13:50:57 +0100

    Change STATIC_URL to /monumenten/static

--- src/monumenten/settings_common.py
+++ src/monumenten/settings_common.py
@@ -121,7 +121,7 @@ REST_FRAMEWORK = dict(
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/monumenten/static/'
 
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
 
